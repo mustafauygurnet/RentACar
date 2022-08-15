cfa214d7325751f7bf97ec4090f80616d44eb0d1
Mustafa UYGUR <mustafauygur@outlook.com>
2022-08-16 01:08:24 +0300

    Add project files.

---
+++ src/projects/rentACarProject/RentACar.Application/Services/Repositories/IBrandRepository.cs
@@ -0,0 +1,9 @@
+using Core.Persistence.Repositories;
+using RentACar.Domain.Entities;
+
+namespace RentACar.Application.Services.Repositories;
+
+public interface IBrandRepository : IAsyncRepository<Brand>,IRepository<Brand>
+{
+    
+}
---
+++ src/projects/rentACarProject/RentACar.Persistence/Contexts/BaseDbContext.cs
@@ -0,0 +1,42 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using RentACar.Domain.Entities;
+
+namespace RentACar.Persistence.Contexts;
+
+public class BaseDbContext : DbContext
+{
+    private IConfiguration Configuration { get; set; }
+    public DbSet<Brand> Brands { get; set; }
+       
+
+
+    public BaseDbContext(DbContextOptions dbContextOptions, IConfiguration configuration) : base(dbContextOptions)
+    {
+        Configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        //if (!optionsBuilder.IsConfigured)
+        //    base.OnConfiguring(
+        //        optionsBuilder.UseSqlServer(Configuration.GetConnectionString("SomeConnectionString")));
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Brand>(a =>
+        {
+            a.ToTable("Brands").HasKey(k => k.Id);
+            a.Property(p => p.Id).HasColumnName("Id");
+            a.Property(p => p.Name).HasColumnName("Name");
+        });
+
+
+
+        Brand[] brandEntitySeeds = { new(1, "Audi"), new(2, "Mercedes") };
+        modelBuilder.Entity<Brand>().HasData(brandEntitySeeds);
+
+           
+    }
+}
